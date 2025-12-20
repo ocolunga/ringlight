8b6eb75f1bd58a22af8a06ac81447ebea27d9bcd
Om Sarraf <itsomsarraf@gmail.com>
2025-12-20 11:18:00 +0530

    Initial Commit

---
+++ ringlight/ringlightApp.swift
@@ -0,0 +1,17 @@
+//
+//  ringlightApp.swift
+//  ringlight
+//
+//  Created by Om Sarraf on 20/12/25.
+//
+
+import SwiftUI
+
+@main
+struct ringlightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ringlightTests/ringlightTests.swift
@@ -0,0 +1,17 @@
+//
+//  ringlightTests.swift
+//  ringlightTests
+//
+//  Created by Om Sarraf on 20/12/25.
+//
+
+import Testing
+@testable import ringlight
+
+struct ringlightTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
